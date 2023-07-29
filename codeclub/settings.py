1a76b810cf31e1d169ea4937b79ecba49b2fd2d2
Thomas <88600695+7amota@users.noreply.github.com>
2023-07-29 20:17:33 +0300

    Update settings.py

--- codeclub/settings.py
+++ codeclub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)9f_$ylwi&j5r3zqglf)(wte0b3(3n0)8fzwciou9#r#&(mcl'
+SECRET_KEY = os.environ.get("SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER)
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD)
